117f164b0b951e3dc60de9262dc8b599720ccfdf
John Hester <hesterj@etableau.com>
2025-10-15 18:47:58 -0500

    chore: simple channel

--- channels.scm
+++ channels.scm
@@ -1,4 +1,12 @@
-(cons* (channel
-         (name 'longleaf)
-         (url "https://github.com/hesterjeng/longleaf-channel"))
-       %default-channels)
+(
+ list 
+(channel
+         (name 'singlecommit_rebase)
+         (url "https://codeberg.org/scrambler/guix")
+         (branch "singlecommit_rebase"))
+)
+;; (cons*
+;; (channel
+;;          (name 'longleaf)
+;;          (url "https://github.com/hesterjeng/longleaf-channel"))
+;;        %default-channels)
