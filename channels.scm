0f9dc65992d91c0dcbeed2b9dcbc0487d9dbd282
John Hester <hesterj@etableau.com>
2025-10-15 02:08:16 -0500

    channel

--- channels.scm
+++ channels.scm
@@ -3,9 +3,9 @@
    (name 'longleaf)
    (url "https://github.com/hesterjeng/longleaf-channel")
  )
- (channel
-   (name 'scrambler-future)
-   (url "https://codeberg.org/scrambler/guix")
-   (branch "future")
- )
+ ;; (channel
+ ;;   (name 'scrambler-future)
+ ;;   (url "https://codeberg.org/scrambler/guix")
+ ;;   (branch "future")
+ ;; )
 %default-channels)
