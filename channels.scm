ba1df915a9e72924d12366407d00f198c7ecdd1e
John Hester <hesterj@etableau.com>
2025-10-16 23:37:19 -0500

    new channels

--- channels.scm
+++ channels.scm
@@ -1,16 +1,19 @@
-(
- list 
+;; (
+;;  list 
+;; (channel
+;;          (name 'guix)
+;;          (url "https://codeberg.org/miramar/guix")
+;;          ;; (url (string-append "file://" (getenv "HOME") "/Projects/guix"))
+;;          (branch "future"))
+;; )
+(cons*
 (channel
-         (name 'guix)
-         (url "https://codeberg.org/miramar/guix")
-         ;; (url (string-append "file://" (getenv "HOME") "/Projects/guix"))
-         (branch "future")))
-;; (channel
-;;          (name 'nonguix)
-;;          (url "https://gitlab.com/nonguix/nonguix")
-;;          (branch "master"))
-;; (cons*
-;; (channel
-;;          (name 'longleaf)
-;;          (url "https://github.com/hesterjeng/longleaf-channel"))
-;;        %default-channels)
+         (name 'longleaf)
+         (url "https://github.com/hesterjeng/longleaf-channel"))
+(channel
+         (name 'nonguix)
+         (url "https://gitlab.com/nonguix/nonguix")
+         (branch "master"))
+;; ()
+%default-channels
+)
